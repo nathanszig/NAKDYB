a6ce92d3e5a8eb2ef1a514ad5889d6225e503b0d
Karen160 <karen.azoulay@edu.devinci.fr>
2024-02-27 17:05:19 +0100

    :lipstick: Bouton

--- front/components/atom/Bouton.vue
+++ front/components/atom/Bouton.vue
@@ -3,7 +3,7 @@ defineProps({
   type: {
     type: String,
     required: false,
-    default: 'text'
+    default: 'button'
   },
   name: {
     type: String,
@@ -14,15 +14,24 @@ defineProps({
     required: false,
     default: false
   },
-  color: {
-
-  }
+  // Savoir si c'est un player ou un admin
+  isPlayer: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  // Savoir si l'état du bouton est actif ou non pour changer la css
+  isActive: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
 })
 
 </script>
 
 <template>
-  <input class="bouton" :type="type" :name="name" :required="required"/>
+  <input :class="`bouton ${isPlayer ? 'player' : 'admin'} ${isActive ? 'active' : ''}`" :type="type" :value="name" :required="required"/>
 </template>
 
 <style scoped lang="scss">
@@ -39,8 +48,32 @@ defineProps({
     }
   }
   &[type='button']{
-
+    width: 230px;
+    height: 62px;
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: bold;
+    &.admin{
+      border: 1px solid var(--blue-500);
+      color: var(--blue-500);
+      &.active, &:hover{
+        background-color: var(--blue-500);
+        color: var(--white-100);
+        filter: drop-shadow(0 0 0.75rem var(--blue-500));
+      }
+    }
+    &.player{
+      border: 1px solid var(--orange-500);
+      color: var(--orange-500);
+      &.active, &:hover{
+        background-color: var(--orange-500);
+        color: var(--white-100);
+        filter: drop-shadow(0 0 0.75rem var(--orange-500));
+      }
+    }
+    &:hover{
+      cursor: pointer;
+    }
   }
-
 }
 </style>
