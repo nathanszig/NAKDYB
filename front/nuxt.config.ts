375a657b9f23d345dd32fba2633689d5270fed30
Karen160 <karen.azoulay@edu.devinci.fr>
2024-02-27 14:25:59 +0100

    :bento: Colors & font

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+                    @use "@/assets/scss/utils/variables.scss" as *;`,
+        },
+      },
+    },
+  },
 })
