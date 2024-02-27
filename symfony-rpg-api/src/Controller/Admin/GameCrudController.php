db9de0ff44683a0fb2659c47c2a4e3bb03a62787
Alexandre Caramel <caramel.alexandre@gmail.com>
2024-02-27 14:03:05 +0100

    FIx association table

--- symfony-rpg-api/src/Controller/Admin/GameCrudController.php
+++ symfony-rpg-api/src/Controller/Admin/GameCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class GameCrudController extends AbstractCrudController
 {
