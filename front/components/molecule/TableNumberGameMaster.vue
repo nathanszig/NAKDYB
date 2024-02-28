08495a5c268eb0229edbc9bf5d2ac1774ffe4ec0
Karen160 <karen.azoulay@edu.devinci.fr>
2024-02-28 14:05:53 +0100

    :sparkles: TableNumberGameMaster

--- front/components/molecule/TableNumberGameMaster.vue
+++ front/components/molecule/TableNumberGameMaster.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 defineProps({
   // Table contents
-  player: {
+  table: {
     type: Array,
     required: true
   },
+  // All features
   features: {
     type: Array,
     required: true
@@ -19,9 +20,42 @@ defineProps({
 </script>
 
 <template>
-
+  <table class="table-number-game-master">
+    <tr>
+      <th></th>
+      <th v-for="title in features">{{ title }}</th>
+    </tr>
+    <tr v-for="content in table">
+      <td>{{content.name}}</td>
+      <td>{{ content.physical }}</td>
+      <td>{{ content.mental }}</td>
+      <td>{{ content.social }}</td>
+    </tr>
+  </table>
 </template>
 
 <style scoped lang="scss">
-
+.table-number-game-master{
+  color: var(--white-100);
+  tr{
+    background-color: var(--blue-900);
+    &:nth-child(odd) {
+      background-color: var(--blue-700);
+    }
+    th{
+      padding: 10px 0;
+      text-transform: uppercase;
+      font-size: 20px;
+      font-weight: bold;
+      width: 110px;
+      border-right: 1px solid var(--blue-500);
+    }
+    td{
+      padding: 10px;
+      font-size: 20px;
+      font-weight: bold;
+      border-right: 1px solid var(--blue-500);
+    }
+  }
+}
 </style>
--- front/pages/test.vue
+++ front/pages/test.vue
@@ -29,12 +29,43 @@ const stats = ref({
   smart: 6,
   chance: 5,
 });
+
+
+const features = ["physical", "mental", "social"]
+
+const tableMember = [
+  {
+    id: 0,
+    name: "Karen",
+    role: "string",
+    physical: 5,
+    mental: 2,
+    social: 0,
+  },
+  {
+    id: 0,
+    name: "Nathan",
+    role: "string",
+    physical: 8,
+    mental: 2,
+    social: 10,
+  },
+  {
+    id: 0,
+    name: "Benji",
+    role: "string",
+    physical: 0,
+    mental: 1,
+    social: 6,
+  },
+]
 </script>
 
 <template>
   <Button name="test"/>
   <TableText :table="table"/>
   <TableNumberPlayer v-model="stats" />
+  <TableNumberGameMaster :features="features" :table="tableMember" />
 </template>
 
 <style scoped>
