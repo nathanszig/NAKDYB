de4b2035d9c4d479f64f53a39e71f93acf8e6147
Karen160 <karen.azoulay@edu.devinci.fr>
2024-02-28 11:21:58 +0100

    :construction: TableNumberGameMaster

---
+++ front/components/molecule/TableNumberGameMaster.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  // Table contents
+  player: {
+    type: Array,
+    required: true
+  },
+  features: {
+    type: Array,
+    required: true
+  },
+  // Know if it's a player or an admin
+  isPlayer: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
